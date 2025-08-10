98a8e15181178293ea598c59e4605f28e94c2413
gotayjosue <gotayjosue@gmail.com>
2025-08-10 16:59:42 -0600

    Week 6 code completed

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,5 @@
+using System.Timers;
+
 public class Node
 {
     public int Data { get; set; }
@@ -12,14 +14,21 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (this.Data == value)
+        {
+            // Value already exists in the tree
+            return;
+        }
 
         if (value < Data)
         {
             // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
-            else
-                Left.Insert(value);
+            {
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+            }
         }
         else
         {
@@ -34,12 +43,35 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+        if (this.Data == value)
+        {
+            return true;
+        }
+        else if (Left is not null && Left.Contains(value))
+        {
+            return true;
+        }
+        else if (Right is not null && Right.Contains(value))
+        {
+            return true;
+        }
+
         return false;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+
+        //If left is not null, call GetHeight() on the left node
+        //If left is null, then the height is 0
+        int leftHeight = Left?.GetHeight() ?? 0;
+
+        //If right is not null, call GetHeight() on the right node
+        //If right is null, then the height is 0
+        int rightHeight = Right?.GetHeight() ?? 0;
+
+        // Return the greater of the two heights plus one for the current node
+        return Math.Max(leftHeight, rightHeight) + 1;
     }
 }
